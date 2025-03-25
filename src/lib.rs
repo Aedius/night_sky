9d976c1ce620c33b64d0e9b7331cff97ef31dc20
aedius <Aedius@users.noreply.github.com>
2025-03-26 00:10:35 +0100

    add lens effect

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,16 @@ use wasm_bindgen::prelude::*;
 use web_sys::CanvasRenderingContext2d;
 use web_sys::{ImageData, console};
 
+enum LensEffect {
+    Branch(u32),
+}
+
+struct NightContext {
+    canvas: CanvasRenderingContext2d,
+    rng: ThreadRng,
+    lens: LensEffect,
+}
+
 #[wasm_bindgen]
 pub fn run() {
     let window = web_sys::window().unwrap();
@@ -17,88 +27,64 @@ pub fn run() {
     let global_width: u32 = global_width_f as u32;
     let global_height: u32 = global_height_f as u32;
 
-    let mut rng = rand::rng();
+    let mut ctx = get_context(global_width, global_height);
 
-    let ctx = get_context(
-        global_width,
-        global_height,
-    );
+    generate_background_color(global_width, global_height, &mut ctx);
 
-    generate_background_color(global_width, global_height, &ctx, &mut rng);
+    generate_base_stars(global_width, global_height, &mut ctx);
 
-    generate_base_stars(global_width, global_height, &ctx, &mut rng);
+    generate_cluster_stars(global_width, global_height, &mut ctx);
 
-    generate_cluster_stars(global_width, global_height, &ctx, &mut rng);
-
-    generate_closest_stars(global_width, global_height, &ctx, &mut rng);
+    generate_closest_stars(global_width, global_height, &mut ctx);
 }
 
-fn generate_closest_stars(
-    global_width: u32,
-    global_height: u32,
-    ctx: &CanvasRenderingContext2d,
-    mut rng: &mut ThreadRng,
-) {
-    for _ in 0..rng.random_range(10..40) {
+fn generate_closest_stars(global_width: u32, global_height: u32, ctx: &mut NightContext) {
+    for _ in 0..ctx.rng.random_range(10..40) {
         let p = Point {
             kind: Big,
-            x: rng.random_range(0..global_width) as f64,
-            y: rng.random_range(0..global_height) as f64,
+            x: ctx.rng.random_range(0..global_width) as f64,
+            y: ctx.rng.random_range(0..global_height) as f64,
         };
-        p.draw(&ctx, &mut rng);
+        p.draw(ctx);
     }
 }
 
-fn generate_cluster_stars(
-    global_width: u32,
-    global_height: u32,
-    ctx: &CanvasRenderingContext2d,
-    mut rng: &mut ThreadRng,
-) {
-    for _ in 0..rng.random_range(15..40) {
+fn generate_cluster_stars(global_width: u32, global_height: u32, ctx: &mut NightContext) {
+    for _ in 0..ctx.rng.random_range(15..40) {
         let p = Point {
             kind: Small,
-            x: rng.random_range(0..global_width) as f64,
-            y: rng.random_range(0..global_height) as f64,
+            x: ctx.rng.random_range(0..global_width) as f64,
+            y: ctx.rng.random_range(0..global_height) as f64,
         };
-        let size = rng.random_range(40. ..75.);
-        for _ in 5..rng.random_range(10..50) {
+        let size = ctx.rng.random_range(40. ..75.);
+        for _ in 5..ctx.rng.random_range(10..50) {
             let np = Point {
                 kind: Small,
-                x: p.x + rng.random_range(-size..size),
-                y: p.y + rng.random_range(-size..size),
+                x: p.x + ctx.rng.random_range(-size..size),
+                y: p.y + ctx.rng.random_range(-size..size),
             };
-            np.draw(&ctx, &mut rng);
+            np.draw(ctx);
         }
     }
 }
 
-fn generate_base_stars(
-    global_width: u32,
-    global_height: u32,
-    ctx: &CanvasRenderingContext2d,
-    mut rng: &mut ThreadRng,
-) {
-    for _ in 0..global_width * global_height / rng.random_range(300..900) {
+fn generate_base_stars(global_width: u32, global_height: u32, ctx: &mut NightContext) {
+    for _ in 0..global_width * global_height / ctx.rng.random_range(300..900) {
         let p = Point {
             kind: Small,
-            x: rng.random_range(0..global_width) as f64,
-            y: rng.random_range(0..global_height) as f64,
+            x: ctx.rng.random_range(0..global_width) as f64,
+            y: ctx.rng.random_range(0..global_height) as f64,
         };
 
-        p.draw(&ctx, &mut rng);
+        p.draw(ctx);
     }
 }
 
-fn generate_background_color(
-    global_width: u32,
-    global_height: u32,
-    ctx: &CanvasRenderingContext2d,
-    rng: &mut ThreadRng,
-) {
+fn generate_background_color(global_width: u32, global_height: u32, ctx: &mut NightContext) {
     // based on : https://gist.github.com/donpark/1796361
 
     let image_data = ctx
+        .canvas
         .get_image_data(0., 0., global_width as f64, global_height as f64)
         .map_err(|e| console::log_1(&e))
         .unwrap();
@@ -106,18 +92,18 @@ fn generate_background_color(
     let mut data = image_data.data();
 
     let alpha = 230u8;
-    let base1 = rng.random_range(10u8..80u8);
-    let base2 = rng.random_range(10u8..80u8);
-    let base3 = rng.random_range(10u8..80u8);
+    let base1 = ctx.rng.random_range(10u8..80u8);
+    let base2 = ctx.rng.random_range(10u8..80u8);
+    let base3 = ctx.rng.random_range(10u8..80u8);
 
     for (pos, chan) in data.iter_mut().enumerate() {
         match (pos + 1) % 4 {
             0 => {
                 *chan = alpha;
             }
-            1 => *chan = rng.random_range(0u8..base1),
-            2 => *chan = rng.random_range(0u8..base2),
-            _ => *chan = rng.random_range(0u8..base3),
+            1 => *chan = ctx.rng.random_range(0u8..base1),
+            2 => *chan = ctx.rng.random_range(0u8..base2),
+            _ => *chan = ctx.rng.random_range(0u8..base3),
         }
     }
 
@@ -127,42 +113,41 @@ fn generate_background_color(
             .map_err(|e| console::log_1(&e))
             .unwrap();
 
-    ctx.put_image_data(&image_data, 0., 0.)
+    ctx.canvas
+        .put_image_data(&image_data, 0., 0.)
         .map_err(|e| console::log_1(&e))
         .unwrap();
 
-    ctx.save();
+    ctx.canvas.save();
 
     let mut size = 8;
 
     while size < global_width {
         let x = random_range(0..global_width - size);
         let y = random_range(0..global_height - size);
-        ctx.set_global_alpha(4. / size as f64);
-        ctx.draw_image_with_html_canvas_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
-            &ctx.canvas().unwrap(),
-            x as f64,
-            y as f64,
-            size as f64,
-            size as f64,
-            0.,
-            0.,
-            global_width as f64,
-            global_height as f64,
-        )
-        .map_err(|e| console::log_1(&e))
-        .unwrap();
+        ctx.canvas.set_global_alpha(4. / size as f64);
+        ctx.canvas
+            .draw_image_with_html_canvas_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
+                &ctx.canvas.canvas().unwrap(),
+                x as f64,
+                y as f64,
+                size as f64,
+                size as f64,
+                0.,
+                0.,
+                global_width as f64,
+                global_height as f64,
+            )
+            .map_err(|e| console::log_1(&e))
+            .unwrap();
 
         size *= 4;
     }
 
-    ctx.restore()
+    ctx.canvas.restore()
 }
 
-fn get_context(
-    global_width: u32,
-    global_height: u32,
-) -> CanvasRenderingContext2d {
+fn get_context(global_width: u32, global_height: u32) -> NightContext {
     let document = web_sys::window().unwrap().document().unwrap();
     let canvas = document.get_element_by_id("canvas").unwrap();
 
@@ -181,69 +166,96 @@ fn get_context(
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
 
-    ctx
+    NightContext {
+        canvas: ctx,
+        rng: rand::rng(),
+        lens: LensEffect::Branch(4),
+    }
 }
 
 impl Point {
-    fn draw(&self, ctx: &CanvasRenderingContext2d, rng: &mut ThreadRng) {
+    fn draw(&self, ctx: &mut NightContext) {
         match self.kind {
-            Small => self.draw_small(ctx, rng),
-            Big => self.draw_big(ctx, rng),
+            Small => self.draw_small(ctx),
+            Big => self.draw_big(ctx),
         }
     }
 
-    fn draw_small(&self, ctx: &CanvasRenderingContext2d, rng: &mut ThreadRng) {
+    fn draw_small(&self, ctx: &mut NightContext) {
         let color = format!(
             "rgb({r},{g},{b})",
-            r = rng.random_range(150..255),
-            g = rng.random_range(150..255),
-            b = rng.random_range(150..255)
+            r = ctx.rng.random_range(150..255),
+            g = ctx.rng.random_range(150..255),
+            b = ctx.rng.random_range(150..255)
         );
 
-        ctx.set_shadow_color(&color);
-        ctx.set_shadow_blur(1.);
-        ctx.set_fill_style_str(&color);
+        ctx.canvas.set_shadow_color(&color);
+        ctx.canvas.set_shadow_blur(1.);
+        ctx.canvas.set_fill_style_str(&color);
 
-        let size = rng.random_range(0.1..1.9);
-        ctx.fill_rect(self.x, self.y, size, size);
+        let size = ctx.rng.random_range(0.1..1.9);
+        ctx.canvas.fill_rect(self.x, self.y, size, size);
 
-        ctx.set_shadow_blur(0.);
+        ctx.canvas.set_shadow_blur(0.);
     }
-    fn draw_big(&self, ctx: &CanvasRenderingContext2d, rng: &mut ThreadRng) {
-        let size = rng.random_range(2. ..6.);
+    fn draw_big(&self, ctx: &mut NightContext) {
+        let size = ctx.rng.random_range(30. ..60.);
 
         let color_center = format!(
             "rgb({r},{g},{b})",
-            r = rng.random_range(200..255),
-            g = rng.random_range(200..255),
-            b = rng.random_range(200..255)
+            r = ctx.rng.random_range(200..255),
+            g = ctx.rng.random_range(200..255),
+            b = ctx.rng.random_range(200..255)
         );
         let color_middle = format!(
             "rgba({r},{g},{b},0.5)",
-            r = rng.random_range(150..255),
-            g = rng.random_range(150..255),
-            b = rng.random_range(150..255)
+            r = ctx.rng.random_range(150..255),
+            g = ctx.rng.random_range(150..255),
+            b = ctx.rng.random_range(150..255)
         );
         let color_outside = "rgb(0,0,0, 0)";
 
         let gradient = ctx
-            .create_radial_gradient(self.x, self.y, size * 0.8, self.x, self.y, size)
+            .canvas
+            .create_radial_gradient(self.x, self.y, size * 0.01, self.x, self.y, size)
             .map_err(|e| console::log_1(&e))
             .unwrap();
 
-        gradient.add_color_stop(0., &color_center) .map_err(|e|
-            console::log_1(&e)
-        ).unwrap();
-        gradient.add_color_stop(0.5, &color_middle) .map_err(|e|
-            console::log_1(&e)
-        ).unwrap();
-        gradient.add_color_stop(1., color_outside) .map_err(|e|
-            console::log_1(&e)
-        ).unwrap();
+        gradient
+            .add_color_stop(0., &color_center)
+            .map_err(|e| console::log_1(&e))
+            .unwrap();
+        gradient
+            .add_color_stop(0.5, &color_middle)
+            .map_err(|e| console::log_1(&e))
+            .unwrap();
+        gradient
+            .add_color_stop(1., color_outside)
+            .map_err(|e| console::log_1(&e))
+            .unwrap();
 
-        ctx.set_fill_style_canvas_gradient(&gradient);
+        ctx.canvas.set_fill_style_canvas_gradient(&gradient);
 
-        ctx.fill_rect(self.x - size, self.y - size, size * 2., size * 2.);
+        let inner_size = ctx.rng.random_range(0.5..3.);
+
+        match ctx.lens {
+            LensEffect::Branch(4) => {
+                ctx.canvas.begin_path();
+                ctx.canvas.move_to(self.x - inner_size, self.y - inner_size);
+                ctx.canvas.line_to(self.x, self.y - size);
+                ctx.canvas.line_to(self.x + inner_size, self.y - inner_size);
+                ctx.canvas.line_to(self.x + size, self.y);
+                ctx.canvas.line_to(self.x + inner_size, self.y + inner_size);
+                ctx.canvas.line_to(self.x, self.y + size);
+                ctx.canvas.line_to(self.x - inner_size, self.y + inner_size);
+                ctx.canvas.line_to(self.x - size, self.y);
+                ctx.canvas.fill();
+            }
+            _ => {
+                ctx.canvas
+                    .fill_rect(self.x - size, self.y - size, size * 2., size * 2.);
+            }
+        }
     }
 }
 
